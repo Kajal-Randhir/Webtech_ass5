f21e3aa4713e62bf5ba875c92935446b4e6b3f19
Kajal-Randhir <68078097+Kajal-Randhir@users.noreply.github.com>
2021-06-12 19:52:25 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php 
+$database = 'localhost';
+$dbName = 'test'; 
+$dbUser = 'root'; 
+$dbPass = ''; 
+$mysqli = mysqli_connect($database, $dbUser, $dbPass, $dbName);    
+?> 
+© 2021 GitHub, Inc.
